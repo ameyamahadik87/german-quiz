ad25eb033dfe7aad048bed40c24f341d77ff0532
nilesh-sutar <nilesh@ssbainnovations.com>
2021-02-24 17:54:36 +0530

    total calculation

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -13,6 +13,7 @@ export class QuestionsComponent implements OnInit {
   userInfo;
   questions = [
     {
+      'id': 0,
       'question': 'What is Your Name',
       'options': [
         'Ram',
@@ -20,9 +21,11 @@ export class QuestionsComponent implements OnInit {
         'Lakshman',
         'Hanuman'
       ],
-      'answer': 'Ram'
+      'answer': 'Ram',
+      'selectedAns': ''
     },
     {
+      'id': 1,
       'question': 'Question 2',
       'options': [
         '1',
@@ -30,9 +33,11 @@ export class QuestionsComponent implements OnInit {
         '3',
         '4'
       ],
-      'answer': '2'
+      'answer': '2',
+      'selectedAns': ''
     },
     {
+      'id': 2,
       'question': 'Question 3',
       'options': [
         '1',
@@ -40,44 +45,73 @@ export class QuestionsComponent implements OnInit {
         '3',
         '4'
       ],
-      'answer': '3'
+      'answer': '3',
+      'selectedAns': ''
     }
   ]
 
   ngOnInit(): void {
-    this.userInfo = JSON.parse(localStorage.getItem("userInfo"))
+    this.userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (localStorage.getItem("isQuizOpen")) {
+      this.isQuizOpen = JSON.parse(localStorage.getItem("isQuizOpen"))
+    }
     console.log(this.userInfo)
   }
 
-  onOptionChange(option, abc) {
-    console.log(option, abc);
+  onOptionChange(selectedAns, i) {
+    this.questions[i].selectedAns = selectedAns;
+    console.log(selectedAns, this.questions);
+  }
+  isQuizOpen;
+  showQuiz() {
+    this.isQuizOpen = true;
+    localStorage.setItem('isQuizOpen', JSON.stringify(this.isQuizOpen));
+  }
+  
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+
+  }
+
+  submit(questions) {
+    let total = 0;
+    for (let index = 0; index < questions.length; index++) {
+      const element = questions[index];
+      if (element.selectedAns) {
+        if (element.selectedAns === element.answer) {
+          total++;
+        }
+      }
+    }
+    console.log(total)
   }
 
   slidesStore = [
     {
-      id : 1,
-      src : 'http://placehold.it/350x150/000000',
-      alt : 'alt'
+      id: 1,
+      src: 'http://placehold.it/350x150/000000',
+      alt: 'alt'
     },
     {
-      id : 2,
-      src : 'http://placehold.it/350x150/111111',
-      alt : 'alt'
+      id: 2,
+      src: 'http://placehold.it/350x150/111111',
+      alt: 'alt'
     },
     {
-      id : 3,
-      src : 'http://placehold.it/350x150/222222',
-      alt : 'alt'
+      id: 3,
+      src: 'http://placehold.it/350x150/222222',
+      alt: 'alt'
     },
     {
-      id : 4,
-      src : 'http://placehold.it/350x150/333333',
-      alt : 'alt'
+      id: 4,
+      src: 'http://placehold.it/350x150/333333',
+      alt: 'alt'
     }
   ]
 
   customOptions: OwlOptions = {
-    loop: true,
+    loop: false,
     mouseDrag: true,
     autoplay: true,
     autoplayTimeout: 10000,
